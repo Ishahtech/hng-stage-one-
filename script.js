66d3451bb30680bb4ae77b892c6946599fd1a8e4
Ishahtech <salawudeenaishatolabisi@gmail.com>
2023-09-11 12:54:18 +0100

    changes made

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
+function padZero(num) {
+    return num < 10 ? "0" + num : num;
+}
+
 function updateCurrentUTCTime() {
-    const currentUTCTimeInMilliseconds = Date.now();
+    const currentUTCDate = new Date();
+    const hours = padZero(currentUTCDate.getUTCHours());
+    const minutes = padZero(currentUTCDate.getUTCMinutes());
+    const seconds = padZero(currentUTCDate.getUTCSeconds());
+    const milliseconds = padZero(currentUTCDate.getUTCMilliseconds());
+
     const element = document.querySelector('[data-testid="currentUTCTime"]');
     if (element) {
-        element.textContent = `Current UTC Time (milliseconds): ${currentUTCTimeInMilliseconds}`;
+        element.textContent = ` ${hours}:${minutes}:${seconds}.${milliseconds} UTC`;
     }
 }
 
+// Wait for the DOM to be fully loaded
 document.addEventListener('DOMContentLoaded', () => {
-    // Update the time every second (1000 milliseconds)
-    setInterval(updateCurrentUTCTime, 1000);
+    // Call the function to initially display the time
+    updateCurrentUTCTime();
+
+    // Update the time every millisecond (1 millisecond)
+    setInterval(updateCurrentUTCTime, 1);
 });
+
+function getCurrentDayOfWeek() {
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDate = new Date();
+    const dayOfWeekIndex = currentDate.getUTCDay();
+    
+    return daysOfWeek[dayOfWeekIndex];
+}
+
+function displayCurrentDayOfWeek() {
+    const currentDayOfWeek = getCurrentDayOfWeek();
+    const element = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    if (element) {
+        element.textContent = `Today is ${currentDayOfWeek}`;
+    }
+}
+
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+    // Call the function to display the current day of the week
+    displayCurrentDayOfWeek();
+});
+
+
+
+
